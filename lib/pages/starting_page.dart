e71a3b546633a6cd5ade4efa47a0d5940971c054
Jeongwon Park <jepark044@gmail.com>
2021-07-14 17:10:54 +0900

    basic structure set

---
+++ lib/pages/starting_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// 4. car manager 의 첫 화면 구성
+class StartingPage extends StatelessWidget {
+  const StartingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
