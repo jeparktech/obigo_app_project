2b4062e7ac5d10ca3629ddc02fe614c1fa9995c9
Chaelee <chaelee227@gmail.com>
2021-07-19 16:37:54 +0900

    fuelinfobody widget

---
+++ lib/widget/fuel_info_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FuelInfoBody extends StatelessWidget {
+@override
+  Widget build(BuildContext context) {
+    return Container(
+      
+      
+    );
+  }
+}
