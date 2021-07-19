ef681b1d6218ce9d915acba1dce0b88cf7f11a12
Jeongwon Park <jepark044@gmail.com>
2021-07-19 12:15:23 +0900

    add and remove method on fuel informations provider file

--- lib/providers/fuel_informations_provider.dart
+++ lib/providers/fuel_informations_provider.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
 
 import '../model/fuel_information.dart';
+import '../database/fuelDBHelper.dart';
 
 class FuelInformations with ChangeNotifier {
-  List<FuelInformations> _items = [];
+  FuelDBHelper fuelDBHelper = FuelDBHelper();
+  List<FuelInformation>? _items;
 
-  List<FuelInformations> get items {
-    return [..._items];
+  FuelInformations() {
+    fuelDBHelper.fuelInfos.then((list) {
+      _items = list;
+    });
+  }
+
+  List<FuelInformation> get items {
+    return [..._items!];
+  }
+
+  bool isListContains(String date) {
+    bool b = false;
+    _items!.forEach((fuelInfo) {
+      if (fuelInfo.date == date) {
+        b = true;
+      }
+    });
+    return b;
+  }
+
+  void addFuelInformation(FuelInformation fuelInfo) {
+    fuelDBHelper.insertFuelInfo(fuelInfo);
+    if (_items == null) {
+      print('this list is null');
+    } else if (_items!.isEmpty) {
+      _items!.add(fuelInfo);
+    } else {
+      if (isListContains(fuelInfo.date)) {
+        //replace the fuelInformation
+      } else {
+        _items!.add(fuelInfo);
+      }
+    }
+    notifyListeners();
+  }
+
+  void deleteFuelInformation(FuelInformation fuelInfo) {
+    fuelDBHelper.deleteFuelInfo(fuelInfo.date);
   }
 }
