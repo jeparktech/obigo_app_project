e393d6d4741afb0e8f219572ee98017fb09949e0
Chaelee <chaelee227@gmail.com>
2021-07-23 10:29:32 +0900

    edit

--- lib/pages/receipt_history_page.dart
+++ lib/pages/receipt_history_page.dart
@@ -15,31 +15,10 @@ class ReceiptHistoryPage extends StatelessWidget {
           style: TextStyle(color: Colors.white, fontSize: 16),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-          margin: EdgeInsets.all(10),
-          height: 10,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(width: 270,),
-              IconButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed(MonthlyRHPage.routeName);
-                }, 
-                icon: Icon(Icons.list, size: 35, color: Color(0xFFC4C4C4),)
-              ),
-              IconButton(
-                onPressed: () {}, 
-                icon: Icon(Icons.calendar_today_rounded, size: 30, color: Color(0xFF1AB5E6),)
-              ),
-            ],
-          ),
-        ),
+      body:
         Calendar(),
-        ],
-      ),
+        
+
     );
   }
 }
--- lib/widget/calendar.dart
+++ lib/widget/calendar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:obigo_app_project/model/fuel_information.dart';
+import 'package:obigo_app_project/pages/monthly_rh_page.dart';
 
 import 'package:obigo_app_project/providers/event_provider.dart';
 import 'package:obigo_app_project/providers/fuel_informations_provider.dart';
@@ -73,40 +74,60 @@ class _CalenderState extends State<Calendar> {
     Column(
       children: <Widget> [
         Container(
-        padding: EdgeInsets.all(10),
-        child: TableCalendar(
-          calendarStyle: CalendarStyle(
-            isTodayHighlighted: true,
-            selectedDecoration: BoxDecoration(
-              color: Color(0xFF1AB5E6),
-              shape: BoxShape.circle,
-            ),
-            selectedTextStyle: TextStyle(color: Colors.white),
-            todayDecoration: BoxDecoration(
-              color: Colors.blue.shade100,
-              shape: BoxShape.circle,
-            ),
+          margin: EdgeInsets.all(10),
+          height: 10,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(width: 270,),
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(MonthlyRHPage.routeName);
+                }, 
+                icon: Icon(Icons.list, size: 35, color: Color(0xFFC4C4C4),)
+              ),
+              IconButton(
+                onPressed: () {}, 
+                icon: Icon(Icons.calendar_today_rounded, size: 30, color: Color(0xFF1AB5E6),)
+              ),
+            ],
           ),
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          child: TableCalendar(
+            calendarStyle: CalendarStyle(
+              isTodayHighlighted: true,
+              selectedDecoration: BoxDecoration(
+                color: Color(0xFF1AB5E6),
+                shape: BoxShape.circle,
+              ),
+              selectedTextStyle: TextStyle(color: Colors.white),
+              todayDecoration: BoxDecoration(
+                color: Colors.blue.shade100,
+                shape: BoxShape.circle,
+              ),
+            ),
 
-          headerStyle: HeaderStyle(
-            titleCentered: true,
-            titleTextStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            formatButtonVisible: false,
-          ),
+            headerStyle: HeaderStyle(
+              titleCentered: true,
+              titleTextStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              formatButtonVisible: false,
+            ),
 
-          firstDay: kFirstDay,
-          lastDay: kLastDay,
-          focusedDay: _focusedDay,
-          selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
-          calendarFormat: _calendarFormat,
-          onDaySelected: _onDaySelected,
-          eventLoader: _getEventsForDay,
-          onFormatChanged: (format) {
-            if (_calendarFormat != format) {
-              setState(() {
-                _calendarFormat = format;
-              });
-            }
+            firstDay: kFirstDay,
+            lastDay: kLastDay,
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            calendarFormat: _calendarFormat,
+            onDaySelected: _onDaySelected,
+            eventLoader: _getEventsForDay,
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
           },
           onPageChanged: (focusedDay) {
             _focusedDay = focusedDay;
